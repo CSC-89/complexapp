18a537ac51124e2bb3072e83283fadd8fa8b6703
Chris <cris.stephens@gmail.com>
2019-11-27 11:04:54 +0100

    update 27/11/19

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const csrf = require('csurf')
 const app = express()
 const sanitizeHTML = require('sanitize-html')
 
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use('/api', require('./router-api'))
+
 let sessionOptions = session({
     secret: "Javascript is cool",
     store: new MongoStore({client: require('./db')}),
@@ -39,10 +43,9 @@ app.use(function(req, res, next) {
 
 const router = require('./router')
 
-// End of Constants
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+
+
 
 app.use(express.static('public'))
 app.set('views', 'views')
